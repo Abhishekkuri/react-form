ca3eba0bec015fab995c8466dfefb9eff1b973e4
abhishek kuri <abhishekkuri@abhisheks-MacBook-Pro.local>
2020-02-18 02:10:15 +0530

    imported font-awesome

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./Footer.css";
+import {
+  faLinkedin,
+  faFacebookSquare,
+  faTwitterSquare
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <div className="text-center center-block">
           <a href="https://www.facebook.com/abhishek.kuri.7">
-            <i className="fa fa-facebook-square fa-3x social" />
+            <FontAwesomeIcon className="icon" icon={faFacebookSquare} />
           </a>
           <a href="https://twitter.com/twimmercom?lang=en">
-            <i className="fa fa-twitter-square fa-3x social" />
+            <FontAwesomeIcon className="icon" icon={faTwitterSquare} />
           </a>
           <a href="https://www.outlook.com">
-            <i className="fa fa-envelope-square fa-3x social" />
+            <FontAwesomeIcon className="icon" icon={faLinkedin} />
           </a>
           <a href="https://linkedin.com/in/abhishekkuri">
-            <i className="fa fa-linkedin-square fa-3x social" />
+            <FontAwesomeIcon className="icon" icon={faLinkedin} />
           </a>
-        </div>
       </footer>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import "./public/bootstrap-4.3.1-dist/css/bootstrap.min.css";
 
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
